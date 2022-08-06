2a59b071703013c977f9420ffd0626073d3492ab
MRodriguez <marcosrodriguezjimenez@gmail.com>
2022-08-06 22:27:04 +0200

    step5

--- part2/tutorial_p2_1.py
+++ part2/tutorial_p2_1.py
@@ -10,20 +10,26 @@ class MainWindow(QMainWindow):
 
         self.setWindowTitle("My App")
 
-        button = QPushButton("Press Me!")
-        button.setCheckable(True)
-        button.clicked.connect(self.the_button_was_toggled)
-        button.setChecked(self.button_is_checked)
-        button.setText("aaaaaah vamos!")
-        texto = button.text()
+        self.button = QPushButton("Press Me!")
+        self.button.setCheckable(True)
+        self.button.clicked.connect(self.the_button_was_toggled)
+        self.button.setChecked(self.button_is_checked)
+        self.button.setText("aaaaaah vamos!")
+        texto = self.button.text()
         print(texto)
+        self.windowTitleChanged.connect(self.titulocambiado)
+        self.setCentralWidget(self.button)
 
-        self.setCentralWidget(button)
+    def titulocambiado(self):
+        print(" El título cambió!")
 
     def the_button_was_toggled(self, checked):
         self.button_is_checked = checked
+        print(checked, self.button.isChecked())
 
         print(self.button_is_checked)
+
+        self.setWindowTitle("Cambió!")
 
 
 # Launch app

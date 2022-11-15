d77598c76accd1815186372ec50d767c72593658
MRodriguez <marcosrodriguezjimenez@gmail.com>
2022-11-15 13:57:52 +0100

    Creación de la ventana

--- curso_pyside6/main.py
+++ curso_pyside6/main.py
@@ -2,3 +2,22 @@
 #
 # https://github.com/Wanderson-Magalhaes/Curso_PySide6_Em_Portugues
 # https://www.youtube.com/watch?v=_qOnJ7-tJcM&list=PLfQ7GQSrl0_ung_Wt0PpgOICqA8k6dr3i
+
+import sys
+import os
+
+# Imorta Qt Core
+from qt_core import *
+
+from gui.windows.main_window.ui_main_window import *
+
+# Ventana principàañ
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.show()
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    sys.exit(app.exec())

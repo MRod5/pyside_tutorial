cab75f8156b7da3e94dc42ab1cdaeabfaebe81fd
MRodriguez <marcosrodriguezjimenez@gmail.com>
2022-11-15 21:45:14 +0100

    pages añadidas desde qt designer

--- curso_pyside6/gui/windows/main_window/ui_main_window.py
+++ curso_pyside6/gui/windows/main_window/ui_main_window.py
@@ -1,5 +1,6 @@
 # Qt Core
 from qt_core import *
+from gui.pages.ui_pages import Ui_application_pages
 
 # Ventana principal
 class UI_MainWindow(object):
@@ -22,15 +23,80 @@ class UI_MainWindow(object):
             # Contenido
             self.content = QFrame()
             self.content.setStyleSheet("background-color: #282a36")
+            
+            # Barra superior
+            self.top_bar = QFrame()
+            self.top_bar.setMinimumHeight(30)
+            self.top_bar.setMaximumHeight(30)
+            self.top_bar.setStyleSheet("background-color: #21232d; color:#6272a4")
+            self.top_bar_layout = QHBoxLayout(self.top_bar)
+            self.top_bar_layout.setContentsMargins(10,0,20,0)
+
+             # Etiqueta izquierda
+            self.top_label_left = QLabel("Aplicación de ejemplo con PySide6")
+
+            # Top Spacer
+            self.top_spacer = QSpacerItem(20, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+
+            # Etiqueta derecha
+            self.top_label_right = QLabel("| Página Principal")
+            self.top_label_right.setStyleSheet("font: 700 9pt 'SegoeUI'")
+
+
+            # Páginas de la aplicación
+            self.pages = QStackedWidget()
+            self.pages.setStyleSheet("font-size: 12pt; color: #f8f8f2; background-color: #282a36")
+            self.ui_pages = Ui_application_pages()
+            self.ui_pages.setupUi(self.pages)
+            self.pages.setCurrentWidget(self.ui_pages.page_1)
+
+            # Barra inferior
+            self.bottom_bar = QFrame()
+            self.bottom_bar.setMinimumHeight(30)
+            self.bottom_bar.setMaximumHeight(30)
+            self.bottom_bar.setStyleSheet("background-color: #21232d; color:#6272a4")
+            self.bottom_bar_layout = QHBoxLayout(self.bottom_bar)
+            self.bottom_bar_layout.setContentsMargins(10,0,20,0)
+
+             # Etiqueta izquierda
+            self.bottom_bar_label_left = QLabel("MRodriguez")
+
+            # Top Spacer
+            self.bottom_bar_spacer = QSpacerItem(20, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+
+            # Etiqueta derecha
+            self.bottom_bar_label_right = QLabel("2022")
+            self.bottom_bar_label_right.setStyleSheet("font: 700 9pt 'SegoeUI'")
+           
+            #añadir a layout barra superior
+            self.bottom_bar_layout.addWidget(self.bottom_bar_label_left)
+            self.bottom_bar_layout.addItem(self.bottom_bar_spacer)
+            self.bottom_bar_layout.addWidget(self.bottom_bar_label_right)
+
+            #añadir a layout barra superior
+            self.top_bar_layout.addWidget(self.top_label_left)
+            self.top_bar_layout.addItem(self.top_spacer)
+            self.top_bar_layout.addWidget(self.top_label_right)
+            
+            # Content Layout
+            self.content_layout =QVBoxLayout(self.content)
+            self.content_layout.setContentsMargins(0,0,0,0)
+            self.content_layout.setSpacing(0)
 
             # Menú izquierdo
             self.left_menu = QFrame()
             self.left_menu.setStyleSheet("background-color: #44475a")
             self.left_menu.setMaximumWidth(50)
+            self.left_menu.setMinimumWidth(50)
 
             # Añade widgets
             self.main_layout.addWidget(self.left_menu)
             self.main_layout.addWidget(self.content)
+
+            # Añade widgets en contenido central
+            self.content_layout.addWidget(self.top_bar)
+            self.content_layout.addWidget(self.pages)
+            self.content_layout.addWidget(self.bottom_bar)
 
             # Widget central:
             parent.setCentralWidget(self.central_frame)

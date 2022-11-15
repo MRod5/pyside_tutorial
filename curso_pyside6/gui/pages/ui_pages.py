b7421b3e8af93f56d90d865495aebe8309f31f22
MRodriguez <marcosrodriguezjimenez@gmail.com>
2022-11-15 21:46:02 +0100

    pages añadidas desde qt designer

---
+++ curso_pyside6/gui/pages/ui_pages.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'pagesQKoiWJ.ui'
+##
+## Created by: Qt User Interface Compiler version 6.2.3
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QGridLayout, QHBoxLayout, QLabel,
+    QSizePolicy, QStackedWidget, QVBoxLayout, QWidget)
+
+class Ui_application_pages(object):
+    def setupUi(self, application_pages):
+        if not application_pages.objectName():
+            application_pages.setObjectName(u"application_pages")
+        application_pages.resize(975, 739)
+        self.page_2 = QWidget()
+        self.page_2.setObjectName(u"page_2")
+        self.verticalLayout = QVBoxLayout(self.page_2)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.label_2 = QLabel(self.page_2)
+        self.label_2.setObjectName(u"label_2")
+
+        self.verticalLayout.addWidget(self.label_2)
+
+        application_pages.addWidget(self.page_2)
+        self.page_3 = QWidget()
+        self.page_3.setObjectName(u"page_3")
+        self.gridLayout = QGridLayout(self.page_3)
+        self.gridLayout.setObjectName(u"gridLayout")
+        self.label = QLabel(self.page_3)
+        self.label.setObjectName(u"label")
+        self.label.setAlignment(Qt.AlignBottom|Qt.AlignRight|Qt.AlignTrailing)
+
+        self.gridLayout.addWidget(self.label, 0, 0, 1, 1)
+
+        application_pages.addWidget(self.page_3)
+        self.page_1 = QWidget()
+        self.page_1.setObjectName(u"page_1")
+        self.horizontalLayout = QHBoxLayout(self.page_1)
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.label_3 = QLabel(self.page_1)
+        self.label_3.setObjectName(u"label_3")
+        self.label_3.setAlignment(Qt.AlignCenter)
+
+        self.horizontalLayout.addWidget(self.label_3)
+
+        application_pages.addWidget(self.page_1)
+
+        self.retranslateUi(application_pages)
+
+        application_pages.setCurrentIndex(2)
+
+
+        QMetaObject.connectSlotsByName(application_pages)
+    # setupUi
+
+    def retranslateUi(self, application_pages):
+        application_pages.setWindowTitle(QCoreApplication.translate("application_pages", u"StackedWidget", None))
+        self.label_2.setText(QCoreApplication.translate("application_pages", u"p2", None))
+        self.label.setText(QCoreApplication.translate("application_pages", u"p3", None))
+        self.label_3.setText(QCoreApplication.translate("application_pages", u"p1 inicial", None))
+    # retranslateUi
+

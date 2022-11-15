352877d22146e0d580b0c98dd5226241100bb930
MRodriguez <marcosrodriguezjimenez@gmail.com>
2022-11-15 13:41:58 +0100

    Cabeceras con enlace gihub y youtube

---
+++ curso_pyside6/main.py
@@ -0,0 +1,4 @@
+# ## Curso pyside6 por Wanderson Magalhaes
+#
+# https://github.com/Wanderson-Magalhaes/Curso_PySide6_Em_Portugues
+# https://www.youtube.com/watch?v=_qOnJ7-tJcM&list=PLfQ7GQSrl0_ung_Wt0PpgOICqA8k6dr3i

fd78ef0bd8c99121cc28dfcc79fb2c18c22de55a
MRodriguez <marcosrodriguezjimenez@gmail.com>
2022-12-13 13:16:57 +0100

    Personalización del estilo del boton

--- curso_pyside6/gui/widgets/py_push_button.py
+++ curso_pyside6/gui/widgets/py_push_button.py
@@ -21,4 +21,59 @@ class PyPushButton(QPushButton):
     ):
 
         super().__init__()
-        return
+
+        # Set default parametros
+        self.setText(texto)
+        self.setMaximumHeight(height)
+        self.setMinimumHeight(height)
+        self.setCursor(Qt.PointingHandCursor)
+
+        # Parametros customizados
+        self.minimum_width = minimum_width
+        self.text_padding = txt_padding
+        self.text_color = text_color
+        self.icon_path = icon_path
+        self.icon_color = icon_color
+        self.btn_color = btn_color
+        self.btn_hover = btn_hover
+        self.btn_pressed = btn_pressed
+        self.is_active = is_active
+
+        # Set style
+        self.set_style(
+            text_padding=self.text_padding,
+            text_color=self.text_color,
+            btn_color = self.btn_color,
+            btn_hover=self.btn_hover,
+            btn_pressed=self.btn_pressed,
+            is_active=self.is_active
+        )
+
+    
+    def set_style(self,
+        txt_padding=55,
+        text_color="#c3ccdf",
+        btn_color="#44475a",
+        btn_hover="#4f5368",
+        btn_pressed="#282a36",
+        is_active = False
+        ):
+        style = f"""
+        QPushbutton {{
+            color: {text_color};
+            background-color: {btn_color};
+            padding-left: {txt_padding}px;
+            text-aling: left;
+            border: none;
+        }}
+        QPushbutton:hover {{
+            background-color: {btn_hover};
+        }}
+        QPushbutton:pressed {{
+            background-color: {btn_pressed};
+        }}
+        """
+
+        self.setStyleSheet(style)
+
+        return None

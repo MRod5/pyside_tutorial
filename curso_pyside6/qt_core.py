daaa64dfaa475ae5a8bc7a674eda7d344ed06104
removedporn <86824510+removedporn@users.noreply.github.com>
2023-03-26 17:37:38 +0800

    Delete curso_pyside6 directory

--- curso_pyside6/qt_core.py
+++
@@ -1,9 +0,0 @@
-# ## Curso pyside6 por Wanderson Magalhaes
-#
-# https://github.com/Wanderson-Magalhaes/Curso_PySide6_Em_Portugues
-# https://www.youtube.com/watch?v=_qOnJ7-tJcM&list=PLfQ7GQSrl0_ung_Wt0PpgOICqA8k6dr3i
-
-from PySide6.QtCore import *
-from PySide6.QtGui import *
-from PySide6.QtWidgets import *
-#from PySide6.QtSvgWidgets import *
